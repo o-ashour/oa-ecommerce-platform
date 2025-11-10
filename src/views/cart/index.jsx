cd4d4939c19a8e7b33810585970163b67cde9d13
Omar Ashour <omar@ashour.ca>
2025-11-10 13:23:47 -0400

    Create Custom Sorting Function to Fix Cart Sorting Issue

--- src/views/cart/index.jsx
+++ src/views/cart/index.jsx
@@ -37,11 +37,26 @@ export default function Cart({ cartItems, setCartItems, data, setData }) {
         { ...foundItem, qtyInCart: foundItem.qtyInCart - 1 },
       ];
     }
-    const sortedCart = updatedCart.sort((a, b) => a.id > b.id);
-    updatedData.sort((a, b) => a.id > b.id);
-    console.log('updatedCart:', updatedCart, 'sortedCart:', sortedCart)
-    setCartItems(sortedCart);
-    setData(updatedData);
+
+    function sortArray(arr) {
+      const sortedArr = [...arr];
+      let temp;
+      for (let i = 0; i < sortedArr.length; i++) {
+        for (let j = 0; j < sortedArr.length - 1; j++) {
+          if (sortedArr[j].id > sortedArr[j + 1].id) {
+            temp = sortedArr[j];
+            sortedArr[j] = sortedArr[j + 1];
+            sortedArr[j + 1] = temp;
+          }
+        }
+      }
+      return sortedArr;
+    }
+
+    const sortedUpdatedCart = sortArray(updatedCart);
+    const sortedUpdatedData = sortArray(updatedData);
+    setCartItems(sortedUpdatedCart);
+    setData(sortedUpdatedData);
   }
 
   return (
