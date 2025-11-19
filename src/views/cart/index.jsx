eda1f51d5da35757b765360f9c58ec6f56cdb705
Omar Ashour <omar@ashour.ca>
2025-11-19 15:32:00 -0400

    Render Product Images in Cart and Add UI for Different States

--- src/views/cart/index.jsx
+++ src/views/cart/index.jsx
@@ -1,12 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
-import imgUrl from "../../assets/images/product_images/Veste en denim classique.png";
 import { changeView } from "../../viewSlice";
 import styles from "./style.module.css";
 import { initializeCart, removeCartItem } from "../../cartSlice";
 import { calculateSubtotal } from "../../utils";
 import { updateStockOnCheckout } from "../../productsSlice";
+import { useState } from "react";
 
 export default function Cart() {
+  const [cartState, setCartState] = useState("");
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   const subtotal = calculateSubtotal(cart);
@@ -18,70 +19,22 @@ export default function Cart() {
   const handleCheckout = () => {
     dispatch(initializeCart());
     dispatch(updateStockOnCheckout(cart));
+    setCartState("paid");
   };
 
   return (
     <div className={styles.cartOuterContainer}>
       <div className={styles.cartInnerContainer}>
         <div className={styles.cartOuterWrapper}>
-          <div className={styles.cartInnerWrapper}>
-            <div className={styles.cartMain}>
-              <div className={styles.cartHeaderWrapper}>
-                <h1>Shopping cart</h1>
-              </div>
-
-              <div className={styles.productItemsSection}>
-                <div className={styles.productItemsWrapper}>
-                  <ul>
-                    {cart.map((cartItem) => (
-                      <li key={cartItem.id}>
-                        <div className={styles.productItemImageWrapper}>
-                          <img alt="product image" src={imgUrl} />
-                        </div>
-
-                        <div className={styles.productItemDetailsWrapper}>
-                          <div className={styles.productItemDetailsTop}>
-                            <h3>{cartItem.name}</h3>
-                            <p>${cartItem.price}</p>
-                          </div>
-                          <div className={styles.productItemDetailsBottom}>
-                            <p>Qty {cartItem.qtyInCart}</p>
-                            <div
-                              className={
-                                styles.productItemDetailsBottomBtnWrapper
-                              }
-                            >
-                              <button
-                                id={cartItem.id}
-                                onClick={() => handleRemoveCartItem(cartItem)}
-                                type="button"
-                              >
-                                Remove
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
+          {cart.length < 1 && !cartState ? (
+            <div className={styles.cartInnerWrapper}>
+              <div className={styles.cartMain}>
+                <div className={styles.cartHeaderWrapper}>
+                  <h1>Your cart is empty</h1>
                 </div>
-              </div>
-            </div>
-
-            <div className={styles.cartFinalizeWrapper}>
-              <div className={styles.cartSubtotalWrapper}>
-                <p>Subtotal</p>
-                <p>${subtotal}</p>
-              </div>
-              <p className={styles.cartSubtotalSubtitle}>
-                Shipping and taxes calculated at checkout.
-              </p>
-              <div className={styles.checkoutBtnWrapper}>
-                <button onClick={handleCheckout}>Checkout</button>
               </div>
               <div className={styles.continueShoppingBtnWrapper}>
                 <p>
-                  or{" "}
                   <button
                     type="button"
                     onClick={() => dispatch(changeView("shop"))}
@@ -92,7 +45,96 @@ export default function Cart() {
                 </p>
               </div>
             </div>
-          </div>
+          ) : cartState === "paid" ? (
+            <div className={styles.cartInnerWrapper}>
+              <div className={styles.cartMain}>
+                <div className={styles.cartHeaderWrapper}>
+                  <h1>Your order has been made!</h1>
+                </div>
+              </div>
+              <div className={styles.continueShoppingBtnWrapper}>
+                <p>
+                  <button
+                    type="button"
+                    onClick={() => dispatch(changeView("shop"))}
+                  >
+                    Continue Shopping
+                    <span> &rarr;</span>
+                  </button>
+                </p>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.cartInnerWrapper}>
+              <div className={styles.cartMain}>
+                <div className={styles.cartHeaderWrapper}>
+                  <h1>Shopping cart</h1>
+                </div>
+
+                <div className={styles.productItemsSection}>
+                  <div className={styles.productItemsWrapper}>
+                    <ul>
+                      {cart.map((cartItem) => (
+                        <li key={cartItem.id}>
+                          <div className={styles.productItemImageWrapper}>
+                            <img alt="product image" src={cartItem.image} />
+                          </div>
+
+                          <div className={styles.productItemDetailsWrapper}>
+                            <div className={styles.productItemDetailsTop}>
+                              <h3>{cartItem.name}</h3>
+                              <p>${cartItem.price}</p>
+                            </div>
+                            <div className={styles.productItemDetailsBottom}>
+                              <p>Qty {cartItem.qtyInCart}</p>
+                              <div
+                                className={
+                                  styles.productItemDetailsBottomBtnWrapper
+                                }
+                              >
+                                <button
+                                  id={cartItem.id}
+                                  onClick={() => handleRemoveCartItem(cartItem)}
+                                  type="button"
+                                >
+                                  Remove
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.cartFinalizeWrapper}>
+                <div className={styles.cartSubtotalWrapper}>
+                  <p>Subtotal</p>
+                  <p>${subtotal}</p>
+                </div>
+                <p className={styles.cartSubtotalSubtitle}>
+                  Shipping and taxes calculated at checkout.
+                </p>
+                <div className={styles.checkoutBtnWrapper}>
+                  <button onClick={handleCheckout}>Checkout</button>
+                </div>
+                <div className={styles.continueShoppingBtnWrapper}>
+                  <p>
+                    or{" "}
+                    <button
+                      type="button"
+                      onClick={() => dispatch(changeView("shop"))}
+                    >
+                      Continue Shopping
+                      <span> &rarr;</span>
+                    </button>
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
